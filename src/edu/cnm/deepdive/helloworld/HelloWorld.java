4601ebef358ba9acd9dc7b7fcafa9c75b1c23281
vvo2 <vuongkv@gmail.com>
2017-12-21 19:06:46 -0500

    First java hello world

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * class for "hello world" message
+ */
+public class HelloWorld {
+
+	/**
+	 * Prints "Hello World!" to standard output
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	System.out.println("Hello World!");
+		
+	}
+
+}
